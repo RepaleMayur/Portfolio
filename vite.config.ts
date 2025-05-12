1f362e77498565b63d3e746f23bf7fef29b200b5
Repale Mayur <mayurrepale7@gmail.com>
2025-05-12 22:48:25 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from 'vite-plugin-static-copy'; // add this plugin
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio/",
   server: {
     host: "::",
     port: 8080,
@@ -12,11 +13,19 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === "development" && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.', // This will copy index.html to 404.html after build
+          rename: '404.html'
+        }
+      ]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Portfolio/", // Ensure assets and routing are relative to the subdirectory on GitHub Pages
 }));
